797437df3d3b590ed3ff1a83a21d17290f02ef9c
Hyecheol (Jerry) Jang <hyecheol.jang@wisc.edu>
2020-08-26 15:34:27 +0900

    Search Enabled

--- js/index.js
+++ js/index.js
@@ -58,7 +58,27 @@ function search(){
     })
     console.log(obj);
     if(obj!=undefined){
-        // TODO: Print proper data to the document
+        // Write data to the text box
+        document.getElementById("staticDept").value = obj.dept; // 부서
+        document.getElementById("staticContact").value = obj.contact; // 연락처
+        if(obj.nid == null) { // 주민등록번호
+            document.getElementById("staticNid").value = "N/A";
+        } else {
+            document.getElementById("staticNid").value = obj.nid;
+        }
+        document.getElementById("staticAddr").value = obj.addr; // 주소지
+        document.getElementById("staticAccount").value = obj.account; // 계좌번호
+        if(obj.reg == false) { // 교육청 등록
+            document.getElementById("staticReg").value = "X";
+        } else {
+            document.getElementById("staticReg").value = obj.reg;
+        }
+        if(obj.scrime == true) { // 성범죄자 확인
+            document.getElementById("staticScrime").value = "O";
+        } else {
+            document.getElementById("staticScrime").value = "X";
+        }
+        document.getElementById("memo").value = obj.memo; // 기타 작성 사항
     }else alert("Not Existing Teacher: " + name);
 
 }
